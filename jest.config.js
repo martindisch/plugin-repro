3f184680b18c3906e355d4ec1fdf8ca44a0f8bde
Martin Disch <martindisch@gmail.com>
2023-02-04 11:52:34 +0100

    Create snapshot test

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
+  },
+  modulePathIgnorePatterns: ["dist"],
+};
